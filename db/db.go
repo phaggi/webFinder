9408dafee5adbd5192dbe1bea394a29a4c2d7239
phaggi <phaggi@gmail.com>
2024-11-27 23:04:48 +0300

    prepare db.go for config.json

--- db/db.go
+++ db/db.go
@@ -2,18 +2,35 @@ package db
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 	"webFinder/models"
 
 	_ "github.com/lib/pq"
 )
 
+type Config struct {
+	DBHost     string `json:"db_host"`
+	DBPort     string `json:"db_port"`
+	DBName     string `json:"db_name"`
+	DBUser     string `json:"db_user"`
+	DBPassword string `json:"db_password"`
+}
+
 var db *sql.DB
 
 func InitDB() {
-	var err error
-	db, err = sql.Open("postgres", "postgres://user:password@localhost/dbname?sslmode=disable")
+	config, err := loadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+	}
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		config.DBHost, config.DBPort, config.DBUser, config.DBPassword, config.DBName)
+
+	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,6 +41,27 @@ func InitDB() {
 	}
 
 	fmt.Println("Successfully connected to the database")
+}
+
+func loadConfig() (*Config, error) {
+	configPath := ".secrets/config.json"
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		return nil, fmt.Errorf("config file not found: %s", configPath)
+	}
+
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open config file: %v", err)
+	}
+	defer file.Close()
+
+	var config Config
+	err = json.NewDecoder(file).Decode(&config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse config file: %v", err)
+	}
+
+	return &config, nil
 }
 
 func CreateTask(task models.Task) (int, error) {
